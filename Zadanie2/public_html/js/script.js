0f929f03a25c59fed4032dc35cc99371673561a7
Admin <badamziom@o2.pl>
2014-07-04 22:34:40 +0200

    Created function and good display

--- Zadanie2/public_html/js/script.js
+++ Zadanie2/public_html/js/script.js
@@ -1,42 +1,70 @@
 
 $(document).ready(function() {
 
+    var i = 0;
+
+    function Display(i) {
+        $.ajax({
+            url: 'questions.json',
+            cache: false,
+            data: {cmd: 'getarticlescount'},
+            dataType: 'json',
+            success: $.getJSON("questions.json", function(data) {
+
+                var n = data.Questions.length;
 
 
-    $.ajax({
-        url: 'questions.json',
-        cache: false,
-        data: {cmd: 'getarticlescount'},
-        dataType: 'json',
-        success: $.getJSON("questions.json", function(data) {
-            var Questions = data.Questions[0].Answers[0].id;
 
-            var answers = new Array();
-
-            var n = data.Questions.length;
-
-            answers['name'] = 'Dupa';
-
-
-            for (var i = 0; i < 1; i++) {
                 $('#question').html(data.Questions[i].Question);
-                var l = data.Questions[i].Answers.length
+                var l = data.Questions[i].Answers.length;
+                $('#checkboxes').html(
+                        '<div id="check" class="col-lg-10"></div>');
                 for (var j = 0; j < l; j++) {
-                    $('#radios').append(
-                            '<div class="radio">' +
+                    $('#check').append(
+                            '<div class="checkbox">' +
                             '<label>' +
-                            '<input class="radio' + j + '" type="radio" name="radios" id="optionsRadios" />' +
+                            '<input class="radio' + j + '" type="checkbox" name="checkbox" id="optionsRadios" value="' + j + '" />' +
                             '</label>' +
                             '</div>');
                     $(".radio").val(data.Questions[i].Answers[j].id);
                     $(".radio" + j).after(data.Questions[i].Answers[j].text);
+
+
                 }
-            }
-            $('#number').html(i + '/' + n);
 
-        })
+                $('#number').html(data.Questions[i].id + '/' + n);
 
-    });
+                $('#prev').removeClass('hide');
+                $('#next').removeClass('hide');
+
+                if (i == 0) {
+                    $('#prev').addClass('hide');
+                }
+                if (i == n - 1) {
+                    $('#next').addClass('hide');
+                }
+
+
+
+            })
+
+        });
+    }
+
+    Display(i);
+
+    $('#next').click(function() {
+        i++;
+        event.preventDefault();
+        Display(i);
+    })
+
+    $('#prev').click(function() {
+        i--;
+        event.preventDefault();
+        Display(i);
+    })
+
 
 });
 
